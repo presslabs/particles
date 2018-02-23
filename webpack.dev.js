6e0893068e993fcfdaaf46fecf036bab24397317
Mihai Grescenko <desero@desero.com>
2018-02-23 14:56:01 +0200

    Update dev server path

--- webpack.dev.js
+++ webpack.dev.js
@@ -5,8 +5,8 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './demo',
-    publicPath: '/particles/',
     port: 3000,
     host: '0.0.0.0',
+    openPage: 'particles/',
   },
 })
